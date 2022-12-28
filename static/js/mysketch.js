a1093c0feb413597c573fd7721b091c19d3dc11d
Martin Rodriguez <mtrpdx@gmail.com>
2022-12-27 17:31:44 -0800

    changes weren't saved

--- static/js/mysketch.js
+++ static/js/mysketch.js
@@ -16,10 +16,10 @@ function draw() {
 
   // put drawing code here
     strokeWeight(4);
-    stroke(153);
+    stroke(154);
     let a = 0.0;
     let inc = TWO_PI / 48.0;
-    for (let i = 0; i < 50; i=i+2) {
+    for (let i = 0; i < 48; i=i+2) {
       line(i * 4.8, 110, i *4.8, 110 + sin(a) * 80.0);
       a = a + inc;
     }
