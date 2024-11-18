a24eaa9a6c19dca9a4bb1d34c73c30503c4d6bac
Martin Rodriguez <mtrpdx@gmail.com>
2024-11-18 13:20:12 -0800

    dir locals

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((compile-command . "FORCE=true emacs -Q --batch -l ./build-site.el --funcall mtr/build-site")))
+ (org-mode . ((org-link-file-path-type . relative))))
