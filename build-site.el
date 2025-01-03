9d3832286be561c9d295175b9283f2de4795e71b
Martin Rodriguez <mtrpdx@gmail.com>
2025-01-03 12:54:59 -0800

    change svg social icons and add toggle

--- build-site.el
+++ build-site.el
@@ -135,7 +135,7 @@
                                           ;; (div (@ (class "vertical-line")) "") " "
                                           ;; (div (@ (class "container"))
                                           ;; ,svg-format))))))))
-                                          (span (@ (class "theme-toggle not-selectable"))
+                                          (span (@ (class "theme-toggle"))
                                           (svg (@ (class "theme-toggler")
                                                   (width "24")
                                                   (height "24")
@@ -143,8 +143,8 @@
                                                   (fill "none")
                                                   (xmlns "http://www.w3.org/2000/svg"))
                                                (path (@ (d "M22 41C32.4934 41 41 32.4934 41 22C41 11.5066 32.4934 3 22
-   3C11.5066 3 3 11.5066 3 22C3 32.4934 11.5066 41 22 41ZM7 22C7
-   13.7157 13.7157 7 22 7V37C13.7157 37 7 30.2843 7 22Z")))))))))))))
+                                                         3C11.5066 3 3 11.5066 3 22C3 32.4934 11.5066 41 22 41ZM7 22C7
+                                                         13.7157 13.7157 7 22 7V37C13.7157 37 7 30.2843 7 22Z")))))))))))))
 
 ;; (defun mtr/site-header ()
 ;;   (list `(header (@ (class "site-header"))
@@ -186,20 +186,42 @@
   (list `(div (@ (class "link-row"))
           (div (@ (class "container") (align "center"))
                (a (@ (href "https://github.com/mtrpdx"))
-                  (img (@ (class "feather")
-                          (src ,(concat mtr/site-url "/assets/icons/github.svg"))))) " "
+                  (svg (@ (class "feather")
+                          (xmlns "http://www.w3.org/2000/svg"))
+                          (path (@ (d "M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0
+                                       0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44
+                                       0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16
+                                       2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07
+                                       5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3
+                                       6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"))))) " "
                (a (@ (href "https://gitlab.com/mtrpdx"))
-                  (img (@ (class "feather")
-                          (src ,(concat mtr/site-url "/assets/icons/gitlab.svg"))))) " "
+                  (svg (@ (class "feather")
+                          (xmlns "http://www.w3.org/2000/svg"))
+                          (path (@ (d "M22.65 14.39L12 22.13 1.35 14.39a.84.84 0 0
+                                       1-.3-.94l1.22-3.78 2.44-7.51A.42.42 0 0 1 4.82
+                                       2a.43.43 0 0 1 .58 0 .42.42 0 0 1 .11.18l2.44
+                                       7.49h8.1l2.44-7.51A.42.42 0 0 1 18.6 2a.43.43 0 0 1
+                                       .58 0 .42.42 0 0 1 .11.18l2.44 7.51L23 13.45a.84.84
+                                       0 0 1-.35.94z"))))) " "
                (a (@ (href "https://www.linkedin.com/in/martintrodriguez/"))
-                  (img (@ (class "feather")
-                          (src ,(concat mtr/site-url "/assets/icons/linkedin.svg"))))) " "
+                  (svg (@ (class "feather")
+                          (xmlns "http://www.w3.org/2000/svg"))
+                          (path (@ (d "M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0
+                                       0-2 2v7h-4v-7a6 6 0 0 1 6-6z")))
+                          (rect (@ (x "2") (y "9") (width "4") (height "12")))
+                          (circle (@ (cx "4") (cy "4") (r "2"))))) " "
                (a (@ (href "mailto:mtrpdx@gmail.com"))
-                  (img (@ (class "feather")
-                          (src ,(concat mtr/site-url "/assets/icons/mail.svg"))))) " "
+                  (svg (@ (class "feather")
+                          (xmlns "http://www.w3.org/2000/svg"))
+                          (path (@ (d "M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1
+                                       0-2-.9-2-2V6c0-1.1.9-2 2-2z")))
+                          (polyline (@ (points "22,6 12,13 2,6"))))) " "
                (a (@ (href "https://soundcloud.com/teensbeans"))
-                  (img (@ (class "feather")
-                          (src ,(concat mtr/site-url "/assets/icons/headphones.svg")))))))))
+                  (svg (@ (class "feather")
+                          (xmlns "http://www.w3.org/2000/svg"))
+                          (path (@ (d "M3 18v-6a9 9 0 0 1 18 0v6")))
+                          (path (@ (d "M21 19a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1
+                                       2-2h3zM3 19a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2H3z")))))))))
 
                ;; (a (@ (href "https://github.com/mtrpdx"))
                   ;; (object (@ (class "feather-icons")
