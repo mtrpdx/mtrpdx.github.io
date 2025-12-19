f7a8a059270ae9f545c48747fbb2eb6481153134
Martin Rodriguez <mtrpdx@gmail.com>
2025-12-19 12:33:47 -0800

    add first post wip

--- build-site.el
+++ build-site.el
@@ -119,8 +119,8 @@
 (setq user-mail-address "mtrpdx@gmail.com")
 
 (defvar mtr/site-url (if (string-equal (getenv "CI") "true")
-                        "" ;; Don't hardcode the domain
-                      "http://localhost:8080")
+                         "" ;; Don't hardcode the domain
+                       "http://localhost:8080")
   "The URL for the site being generated.")
 
 (defvar javascript-format
@@ -145,13 +145,15 @@
    (when (eq backend 'html)
      (mtr/embed-video path))))
 
+(setq org-latex-prefer-user-labels t)
+
 ;; Customize the HTML output
 (setq org-html-validation-link nil
       org-html-head-include-scripts nil
       org-html-head-include-default-style nil
       org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
-                      ;; "<script type=\"text/javascript\" src=\"0.0.0.0:8080/assets/js/main.js\" />"))
-      ;; org-html-head "<link rel=\"stylesheet\" href=\"assets/css/style.css\" />")
+;; "<script type=\"text/javascript\" src=\"0.0.0.0:8080/assets/js/main.js\" />"))
+;; org-html-head "<link rel=\"stylesheet\" href=\"assets/css/style.css\" />")
 
 ;; (defun mtr/site-header ()
 ;;   (list `(header (@ (class "site-header"))
@@ -171,17 +173,17 @@
 ;;                                      (a (@ (class "nav-link") (href "/readinglist/")) "Reading List"))))))))
 (defun mtr/site-header ()
   (list `(header (@ (class "site-header"))
-                 (div (@ (class "site-masthead"))
-                      (div (@ (class "container"))
-                           (nav (@ (class "nav"))
-                                (div (@ (class "row"))
-                                     (div (@ (class "column"))
-                                          (a (@ (class "nav-link") (href "/")) "> mtrpdx/") " ")
-                                     (div (@ (class "column align-right"))
-                                          (a (@ (class "nav-link") (href "/about/")) "About") " "
-                                          (a (@ (class "nav-link") (href "/posts/")) "Posts") " "
-                                          (a (@ (class "nav-link") (href "/projects/")) "Projects") " "
-                                          (a (@ (class "nav-link") (href "/readinglist/")) "Reading List") " "))))))))
+          (div (@ (class "site-masthead"))
+               (div (@ (class "container"))
+                    (nav (@ (class "nav"))
+                         (div (@ (class "row"))
+                              (div (@ (class "column"))
+                                   (a (@ (class "nav-link") (href "/")) "> mtrpdx/") " ")
+                              (div (@ (class "column align-right"))
+                                   (a (@ (class "nav-link") (href "/about/")) "About") " "
+                                   (a (@ (class "nav-link") (href "/posts/")) "Posts") " "
+                                   (a (@ (class "nav-link") (href "/projects/")) "Projects") " "
+                                   (a (@ (class "nav-link") (href "/readinglist/")) "Reading List") " "))))))))
 
 (defun mtr/link-row ()
   "Create row for displaying social links. This version uses svgs."
@@ -190,7 +192,7 @@
                (a (@ (href "https://github.com/mtrpdx"))
                   (svg (@ (class "feather")
                           (xmlns "http://www.w3.org/2000/svg"))
-                          (path (@ (d "M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0
+                       (path (@ (d "M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0
                                        0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44
                                        0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16
                                        2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07
@@ -199,7 +201,7 @@
                (a (@ (href "https://gitlab.com/mtrpdx"))
                   (svg (@ (class "feather")
                           (xmlns "http://www.w3.org/2000/svg"))
-                          (path (@ (d "M22.65 14.39L12 22.13 1.35 14.39a.84.84 0 0
+                       (path (@ (d "M22.65 14.39L12 22.13 1.35 14.39a.84.84 0 0
                                        1-.3-.94l1.22-3.78 2.44-7.51A.42.42 0 0 1 4.82
                                        2a.43.43 0 0 1 .58 0 .42.42 0 0 1 .11.18l2.44
                                        7.49h8.1l2.44-7.51A.42.42 0 0 1 18.6 2a.43.43 0 0 1
@@ -208,36 +210,36 @@
                (a (@ (href "https://www.linkedin.com/in/martintrodriguez/"))
                   (svg (@ (class "feather")
                           (xmlns "http://www.w3.org/2000/svg"))
-                          (path (@ (d "M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0
+                       (path (@ (d "M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0
                                        0-2 2v7h-4v-7a6 6 0 0 1 6-6z")))
-                          (rect (@ (x "2") (y "9") (width "4") (height "12")))
-                          (circle (@ (cx "4") (cy "4") (r "2"))))) " "
+                       (rect (@ (x "2") (y "9") (width "4") (height "12")))
+                       (circle (@ (cx "4") (cy "4") (r "2"))))) " "
                (a (@ (href "mailto:mtrpdx@gmail.com"))
                   (svg (@ (class "feather")
                           (xmlns "http://www.w3.org/2000/svg"))
-                          (path (@ (d "M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1
+                       (path (@ (d "M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1
                                        0-2-.9-2-2V6c0-1.1.9-2 2-2z")))
-                          (polyline (@ (points "22,6 12,13 2,6"))))) " "
+                       (polyline (@ (points "22,6 12,13 2,6"))))) " "
                (a (@ (href "https://soundcloud.com/teensbeans"))
                   (svg (@ (class "feather")
                           (xmlns "http://www.w3.org/2000/svg"))
-                          (path (@ (d "M3 18v-6a9 9 0 0 1 18 0v6")))
-                          (path (@ (d "M21 19a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1
+                       (path (@ (d "M3 18v-6a9 9 0 0 1 18 0v6")))
+                       (path (@ (d "M21 19a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1
                                        2-2h3zM3 19a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2H3z")))))))))
 
 (defun mtr/site-footer ()
   (list `(footer (@ (class "site-footer"))
-                 (div (@ (class "container"))
-                      (div (@ (class "row"))
-                           (div (@ (class "column"))
-                          ,@(mtr/link-row)))
-                      (div (@ (class "row"))
-                          (div (@ (class "column"))
-                                ;; (div (@ (class "site-footer-line"))
-                                ;;      " · "
-                                ;;      " · "
-                                (div (@ (class "site-footer-line"))
-                                     "© 2024 · mtrpdx")))))))
+          (div (@ (class "container"))
+               (div (@ (class "row"))
+                    (div (@ (class "column"))
+                         ,@(mtr/link-row)))
+               (div (@ (class "row"))
+                    (div (@ (class "column"))
+                         ;; (div (@ (class "site-footer-line"))
+                         ;;      " · "
+                         ;;      " · "
+                         (div (@ (class "site-footer-line"))
+                              "© 2024 · mtrpdx")))))))
 
 (defun get-article-output-path (org-file pub-dir)
   (let ((article-dir (concat pub-dir
@@ -285,8 +287,8 @@
        (link (@ (rel "stylesheet") (href ,(concat mtr/site-url "/assets/css/code.css"))))
        (link (@ (rel "stylesheet") (href ,(concat mtr/site-url "/assets/css/style.css"))))
        (script (@ (type "text/javascript") (src ,(concat mtr/site-url "/assets/js/main.js")))
-                    ;; Empty string to cause a closing </script> tag
-                    "")
+               ;; Empty string to cause a closing </script> tag
+               "")
 
 
        ,(when head-extra head-extra)
@@ -296,7 +298,7 @@
             (div (@ (class "container"))
                  (div (@ (class "site-post"))
                       (h1 (@ (class "site-post-title"))
-                         ,title)
+                          ,title)
                       ,(when publish-date
                          `(p (@ (class "site-post-meta")) ,publish-date))
                       ,(if-let ((video-id (plist-get info :video)))
@@ -304,8 +306,8 @@
                       ,(when pre-content pre-content)
                       (div (@ (id "content"))
                            ,content)))
-                 ,@(unless exclude-footer
-                     (mtr/site-footer)))))))
+            ,@(unless exclude-footer
+                (mtr/site-footer)))))))
 
 (defun mtr/org-html-link (link contents info)
   "Remove file extension and changes the path into lowercase file:// links."
@@ -370,14 +372,14 @@
 
 (defun mtr/org-html-src-block (src-block _contents info)
   (let* ((lang (org-element-property :language src-block))
-           (code (org-html-format-code src-block info)))
+         (code (org-html-format-code src-block info)))
     (format "<pre>%s</pre>" (string-trim code))))
 
 (defun mtr/org-html-template (contents info)
   (mtr/generate-page (org-export-data (plist-get info :title) info)
-                    contents
-                    info
-                    :publish-date (org-export-data (org-export-get-date info "%B %e, %Y") info)))
+                     contents
+                     info
+                     :publish-date (org-export-data (org-export-get-date info "%B %e, %Y") info)))
 
 (org-export-define-derived-backend 'site-html 'html
   :translate-alist
